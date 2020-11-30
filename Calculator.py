e03bc4cba1f3c0426856f0237f7e80573daf7624
justin stephan <jay_stefan@yahoo.com>
2020-11-30 15:17:43 -0500

    Second Upddate: impletmented '+/-' and 'x'. equation result is now saved for additional immeadiate calculation.  Output of division is now displayed by appropriate type(float or int).  Increase all font size and the dimensions of the calculator window

--- Calculator.py
+++ Calculator.py
@@ -1,26 +1,44 @@
 import tkinter as tk
 
-window = tk.Tk()
+root = tk.Tk()
+root.title('Calculator')
+
 
 numStack = ''
 equation = ''
 def buttonPress(button):
     global numStack
     global equation
+
     if button == 'C':
         numStack, newText, equation = '', '', ''
+
     if button.isnumeric():
         numStack = numStack + button
-    if button == '-' or button =='+' or button =='*' or button =='/':
+
+    if button == '-' or button =='+' or button =='x' or button =='/':
+        if button == 'x':
+            button = '*'
         equation = equation + numStack + button
         numStack = ''
+
     if button == '=':
         equation = equation + numStack
         numStack = str(eval(equation))
+        equation = ''
+        if float(numStack) == int(float(numStack)):
+            numStack = str(int(float(numStack)))
         
-        
+    if button == '+/-':
+        if numStack != '':
+            if '-' in numStack:
+                numStack = numStack.replace('-','')
+            else:
+                numStack = '-' + numStack
+    
     newText = numStack    
     entryText.set(newText)
+    numstack = ''
     
 
 def makeButtons(buttons,row, column):
@@ -28,10 +46,11 @@ def makeButtons(buttons,row, column):
     c = column
     for button in buttons:
         tk.Button(
-            window,
+            root,
+            font = ('helvetica', 14),
             text = button, 
-            width = 5, 
-            height = 2, 
+            width = 7, 
+            height = 3, 
             command = lambda b=button: buttonPress(b)).grid(row = r, column = c)
         r +=1
 def updateEntry(entry):
@@ -39,18 +58,18 @@ def updateEntry(entry):
 
 
 entryText = tk.StringVar()
-entry = tk.Entry(window, textvariable = entryText).grid(row = 0, column = 0, rowspan = 2, columnspan = 5)
+entry = tk.Entry(root, textvariable = entryText, width=17, font = ('helvetica', 24)).grid(row = 0, column = 0, rowspan = 1, columnspan = 4)
+
 column1 = ['','7','4','1','+/-']
-makeButtons(column1,2,0)
-
 column2 = ['','8','5','2','0']
-makeButtons(column2,2,1)
-
 column3 = ['C','9','6','3','']
-makeButtons(column3,2,2)
-
 column4 = ['/','x','-','+','=']
+
+
+makeButtons(column1,2,0)
+makeButtons(column2,2,1)
+makeButtons(column3,2,2)
 makeButtons(column4,2,3)
 
 
-window.mainloop()
+root.mainloop()
